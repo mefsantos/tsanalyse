c92fb075baa5a2153f531c1f7e87e2739bfe4b25
mefsantos <medgarfsantos@gmail.com>
2018-02-28 15:03:21 +0000

    unit test files added

---
+++ tools/unittests/entropyUnit_test.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+import unittest
+
+from .tools import entropy
+import tools.filter
+
+
+class TestEntropyModule(unittest.TestCase):
+    """
+    Tests for the entropy module
+
+    All the test use a predetermined file adulterado in the unittest_dataset_clean
+        
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        if not os.path.exists('unittest_dataset_clean'):
+            os.mkdir('unittest_dataset_clean')
+        tools.filter.ds_filter('unittest_dataset/adulterado.txt', 'unittest_dataset_clean', apply_limits=True)
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree('unittest_dataset_clean')
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False, verbosity=2)
