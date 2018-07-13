d6df58194f50926fa5a005934ea162ff35815c2d
mefsantos <medgarfsantos@gmail.com>
2018-07-13 14:28:25 +0100

    still fixing setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
-#!/usr/bin/env python
+# #!/usr/bin/env python
 
-from setuptools import setup
+# from setuptools import setup
 
 requirements = ""
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
-f.close()
+# f.close()
+
+# # setup(name='TSAnalyse',
+# #       version='1.0',
+# #       description='TSAnalyse - A Framework for Time Series Analysis',
+# #       author='Marcelo Santos',
+# #       author_email='marcelo.santos@fc.up.pt',
+# #       url='https://github.com/mefsantos/tsanalyse',
+# #       packages=['.', 'tools'],
+# #       install_requires=requirements
+# #      )
+
 
 setup(name='TSAnalyse',
       version='1.0',
@@ -13,7 +24,18 @@ setup(name='TSAnalyse',
       author='Marcelo Santos',
       author_email='marcelo.santos@fc.up.pt',
       url='https://github.com/mefsantos/tsanalyse',
-      packages=['.', 'tools'],
+      packages=['tools'],
       install_requires=requirements
      )
 
+
+
+# from distutils.core import setup
+
+# setup(name='TSAnalyse',
+#       maintainer='Marcelo Santos',
+#       maintainer_email='marcelo.santos@fc.up.pt',
+#       url='https://github.com/mefsantos/tsanalyse',
+#       version='1.0',
+#       py_modules=['TSAnalyseDirect','TSAnalyseMultiScale','TSAnalyseFileBlocks'],
+#       )
