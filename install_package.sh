f9919457111be88d77b4c73b89ecd810c53c4042
mefsantos <medgarfsantos@gmail.com>
2018-07-19 14:46:23 +0100

    change installation script: no longer need to reload bash

--- install_package.sh
+++ install_package.sh
@@ -24,13 +24,14 @@ echo "#add paq8l and ppmd source files to PATH" >> ~/.bashrc
 
 PACKAGE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-
 echo export PATH='${PATH}':${PACKAGE_DIR}/algo/paq8l_src >> ~/.bashrc
 echo export PATH='${PATH}':${PACKAGE_DIR}/algo/ppmd_src >> ~/.bashrc
 echo -e "\n" >> ~/.bashrc
 
+export PATH=$PATH:$PACKAGE_DIR/algo/paq8l_src
+export PATH=$PATH:$PACKAGE_DIR/algo/ppmd_src
+
 echo ""
 echo "Done"
 
-bash # reload environment
 
