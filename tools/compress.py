b9c66ae7a2a682402909126af3ef5489d92c33f7
mefsantos <medgarfsantos@gmail.com>
2018-07-24 17:30:12 +0100

    Debugging available compressors

--- tools/compress.py
+++ tools/compress.py
@@ -422,15 +422,17 @@ def test_compressors():
         exec_path = exec_path[0].split(':')
     for compressor in compressor_list.keys():
         for dir_in_path in exec_path:
-            print os.path.join(dir_in_path, compressor)
+            # print os.path.join(dir_in_path, compressor)
             if os.path.exists(os.path.join(dir_in_path, compressor)) or os.path.exists(
                     os.path.join(dir_in_path, compressor + '.exe')):
                 available[compressor] = compressor_list[compressor]
+                print "PATH %s EXIST!!!" % os.path.join(dir_in_path, compressor)
     return available
 
 
 # A constant variable with the list of available compressors in the path
 AVAILABLE_COMPRESSORS = test_compressors()
+print AVAILABLE_COMPRESSORS
 
 
 def add_parser_options(parser):
