523af7b194b9e25003cb0d1a9b39691d194574ed
mefsantos <medgarfsantos@gmail.com>
2018-07-24 19:09:12 +0100

    still fixing install

--- install_package.sh
+++ install_package.sh
@@ -24,26 +24,30 @@ cd "$(dirname -- "$0")"
 PACKAGE_DIR="$PWD"
 cd - > /dev/null
 
+# change file to write depending on the OS
+if [[ $OSTYPE == *"linux"* ]]; then
+    FILETOWRITE="$HOME/.bashrc"
+else
+    FILETOWRITE="$HOME/.bash_profile"
+fi
+
+
 # only add to bashrc if path does not exists in $PATH
 if ! echo $PATH | grep -Fq "/algo/paq8l_src"
 then
 	echo "Adding PAQ8L source files to PATH"
-	echo "" >> ~/.bashrc
-	echo "# PAQ8L" >> ~/.bashrc
-	echo export PATH='${PATH}':${PACKAGE_DIR}/algo/paq8l_src >> ~/.bashrc
-	# for macOS add to bash profile
-    echo export PATH='${PATH}':${PACKAGE_DIR}/algo/paq8l_src >> ~/.bash_profile
+	echo "" >> $FILETOWRITE
+	echo "# PAQ8L" >> $FILETOWRITE
+	echo export PATH='${PATH}':${PACKAGE_DIR}/algo/paq8l_src >> $FILETOWRITE
 	export PATH=$PATH:$PACKAGE_DIR/algo/paq8l_src
 fi
 
 if ! echo $PATH | grep -Fq "/algo/ppmd_src"
 then
 	echo "Adding PPMD source files to PATH"
-	echo "" >> ~/.bashrc
-	echo "# PPMD" >> ~/.bashrc
-	echo export PATH='${PATH}':${PACKAGE_DIR}/algo/ppmd_src >> ~/.bashrc
-	# for macOS add to bash profile
-    echo export PATH='${PATH}':${PACKAGE_DIR}/algo/paq8l_src >> ~/.bash_profile
+	echo "" >> $FILETOWRITE
+	echo "# PPMD" >> $FILETOWRITE
+	echo export PATH='${PATH}':${PACKAGE_DIR}/algo/ppmd_src >> $FILETOWRITE
 	export PATH=$PATH:$PACKAGE_DIR/algo/ppmd_src
 fi
 
